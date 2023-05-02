aef094b2b2a1be2375c5d97f6b29a6b624caf78e
ssulphurr <ssulphurr@gmail.com>
2023-05-02 22:10:04 +0300

    adds step 1 without css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,60 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  addFeedback = label => {
+    this.setState(prevState => {
+      console.log(prevState[label]);
+
+      return { [label]: prevState[label] + 1 };
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <h1>Please leave feedback</h1>
+        <button type="button" onClick={() => this.addFeedback('good')}>
+          Good
+        </button>
+        <button type="button" onClick={() => this.addFeedback('neutral')}>
+          Neutral
+        </button>
+        <button type="button" onClick={() => this.addFeedback('bad')}>
+          Bad
+        </button>
+
+        <h1>Statistics</h1>
+        <ul>
+          <li>Good: {this.state.good}</li>
+          <li>Neutral: {this.state.neutral}</li>
+          <li>Bad: {this.state.bad}</li>
+        </ul>
+      </>
+    );
+  }
+}
+
+export default App;
